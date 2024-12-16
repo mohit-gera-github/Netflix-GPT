9b7d752095bae8d260819b69a0acd2954fc5c37b
Mohit Gera <mohitgera7714@gmail.com>
2024-12-16 16:12:52 +0530

    Remove constants from tracking

--- src/components/constant.js
+++
@@ -1,9 +0,0 @@
-export const LOGO = 'https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png'
-export const USER_AVATAR ="https://media.licdn.com/dms/image/v2/D4E03AQHB2qTylkJ79g/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1732023582468?e=1739404800&v=beta&t=azuffUJVSRiL8bXKz3hczPgBRDpVkhn7bSBN-RaIJYk"
-export const API_OPTIONS ={
-    method: 'GET',
-    headers: {
-      accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4MjdmZmJmYTM2MGU3MzcyMzdkNTAxMTRjY2Y1MjBlMSIsIm5iZiI6MTcxMjY2NDE2MC41ODA5OTk5LCJzdWIiOiI2NjE1MmU2MDMzYTUzMzAxNjQ4Njk3ZGMiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.FVP7WSH1nv0WGz5g5lTYWyM5tPUhedUfMjeTeYFhGvw'
-    }
-  };
